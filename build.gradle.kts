2eb6f54e656a7df1afd192103d3cd0dc20f60f7d
Elie G <elyahou.hadass@gmail.com>
2025-01-10 06:43:13 +0200

    Update library version to 0.1.4 and improve app restart logic.
    Bumped the version of dependencies in `build.gradle.kts` and updated examples in the README. Refactored `AppRestarter` to delegate executable path resolution to a new `AppManager` singleton for improved readability and exception handling.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,4 +11,4 @@ subprojects {
     apply(plugin = "org.jetbrains.dokka")
 }
 
-extra["libVersion"] = "0.1.3"
+extra["libVersion"] = "0.1.4"
--- platformtools/appmanager/src/jvmMain/kotlin/io/github/kdroidfilter/platformtools/appmanager/AppRestarter.jvm.kt
+++ platformtools/appmanager/src/jvmMain/kotlin/io/github/kdroidfilter/platformtools/appmanager/AppRestarter.jvm.kt
@@ -3,10 +3,20 @@ package io.github.kdroidfilter.platformtools.appmanager
 import java.io.File
 import kotlin.system.exitProcess
 
+object AppManager {
+    val applicationExecutablePath: String by lazy {
+        try {
+            File(ProcessHandle.current().info().command().get()).absolutePath
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw RuntimeException("Failed to get application executable path")
+        }
+    }
+}
+
 actual fun restartApplication() {
     try {
-        val resolvedExecutable = File(ProcessHandle.current().info().command().get())
-        val processBuilder = ProcessBuilder(resolvedExecutable.absolutePath)
+        val processBuilder = ProcessBuilder(AppManager.applicationExecutablePath)
         processBuilder.start()
         exitProcess(0)
     } catch (e: Exception) {
