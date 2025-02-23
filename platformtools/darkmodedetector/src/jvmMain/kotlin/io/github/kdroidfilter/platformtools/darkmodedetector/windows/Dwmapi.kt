62dd5d74d1ff8b6e4426db5d5f1db3c3d11dd0af
Elie G <elyahou.hadass@gmail.com>
2025-02-23 19:16:52 +0200

    Make DwmApi interface internal for stricter access control
    Changed the visibility of the DwmApi interface to internal to restrict its access within the module. This improves encapsulation and aligns with Kotlin's best practices for interface visibility.

--- platformtools/darkmodedetector/src/jvmMain/kotlin/io/github/kdroidfilter/platformtools/darkmodedetector/windows/Dwmapi.kt
+++ platformtools/darkmodedetector/src/jvmMain/kotlin/io/github/kdroidfilter/platformtools/darkmodedetector/windows/Dwmapi.kt
@@ -6,7 +6,7 @@ import com.sun.jna.Pointer
 import com.sun.jna.platform.win32.WinDef.HWND
 import com.sun.jna.win32.W32APIOptions
 
-interface DwmApi : Library {
+internal interface DwmApi : Library {
     companion object {
         val INSTANCE: DwmApi = Native.load("dwmapi", DwmApi::class.java, W32APIOptions.DEFAULT_OPTIONS)
         const val DWMWA_USE_IMMERSIVE_DARK_MODE = 20
