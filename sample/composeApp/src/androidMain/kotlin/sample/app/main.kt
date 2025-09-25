d30fdb9580193c0b836c38b969a4d07b829fcb05
Elie Gambache <elyahou.hadass@gmail.com>
2025-09-25 23:39:07 +0300

    feat: Initialize Android clipboard monitoring in `AppActivity`
    - Added `ClipboardMonitorFactory.init(this)` call in `AppActivity` to enable clipboard monitoring functionality for Android.

--- sample/composeApp/src/androidMain/kotlin/sample/app/main.kt
+++ sample/composeApp/src/androidMain/kotlin/sample/app/main.kt
@@ -7,6 +7,8 @@ import androidx.activity.compose.setContent
 class AppActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        io.github.kdroidfilter.platformtools.clipboardmanager.ClipboardMonitorFactory.init(this)
+
         setContent {
             App()
         }
